d59d0c69e9d17684fda28edcc8f27e934421eb6e
akashsharma135 <akashsharma1352000@gmail.com>
2021-09-08 18:59:17 +0530

    created all routes

--- crudApi/models.py
+++ crudApi/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    product_name = models.CharField(max_length=50, null=False)
+    product_name = models.CharField(max_length=50, null=False, unique=True)
     product_desc = models.CharField(max_length=1000)
     product_quantity = models.IntegerField(default=0, null=False)
     product_price = models.IntegerField(null=False)
